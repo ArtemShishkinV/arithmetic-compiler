bba8649fe29437abf377e0d64eabd75f1824c1b1
g943_shishkin.a.i <astopikov@gmail.com>
2022-11-24 13:53:26 +0300

    implement posftix generator with tokens

--- arithmetic-compiler/internal/postfix/postfixGenerator.go
+++ arithmetic-compiler/internal/postfix/postfixGenerator.go
@@ -4,6 +4,8 @@ import (
 	"arithmetic-compiler/internal/code/models"
 	models2 "arithmetic-compiler/internal/lexical/models"
 	models3 "arithmetic-compiler/internal/syntax/models"
+	"fmt"
+	"golang.org/x/exp/slices"
 	"strings"
 )
 
@@ -28,14 +30,28 @@ func (g *postfixGenerator) getPostfixExpression() []string {
 	var operands []string
 
 	for _, item := range g.actions {
-		operators = append(operators, item.GetOperator().Value)
+		operators = append(operators, fmt.Sprintf("<%s>", item.GetOperator().Value))
 	}
 
-	//for i := len(g.actions) - 1; i >= 0; i-- {
-	//	operands = append(operands, g.actions[i].GetOperands()...)
-	//}
+	tokenOperands := g.filteredOperands()
+	fmt.Println(tokenOperands)
+	for i := len(tokenOperands) - 1; i >= 0; i-- {
+		operands = append(operands, tokenOperands[i].Value)
+	}
 
 	return g.concatOperandsAndOperators(operands, operators)
+}
+
+func (g *postfixGenerator) filteredOperands() []models2.Token {
+	var operands []models2.Token
+	for i := len(g.actions) - 1; i >= 0; i-- {
+		for _, j := range g.actions[i].GetOperands() {
+			if j.Lexeme.Type != models2.Result && !slices.Contains(operands, j) {
+				operands = append(operands, j)
+			}
+		}
+	}
+	return operands
 }
 
 func (g *postfixGenerator) concatOperandsAndOperators(operands []string, operators []string) []string {
